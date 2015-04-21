6104ee12eccb0df8969763415ad521ca1df06c4c
Ian Morrison <ian.morrison.a@gmail.com>
2015-04-21 15:34:08 -0700

    Add gitconfig

--- install.sh
+++ install.sh
@@ -2,8 +2,12 @@
 
 echo "Deleting the old files"
 rm ~/.vimrc
+rm ~/.gitconfig
+
 
 echo "Symlinking Files"
 ln -s ~/Github/dotfiles/vimrc ~/.vimrc
+ln -s ~/Github/dotfiles/gitconfig ~/.gitconfig
+
 
 echo "All done"
