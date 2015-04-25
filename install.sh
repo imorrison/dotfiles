add2da5b1e9fea82582edc8d1965b51576202c7c
Ian Morrison <ian.morrison.a@gmail.com>
2015-04-25 16:44:55 -0700

    Fetch and update submodules on install

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+echo "initializing submodules"
+git submodule init
+git submodule update
+
+
 echo "Deleting the old files"
 rm ~/.vimrc
 rm ~/.vim
@@ -11,5 +16,7 @@ ln -s ~/Github/dotfiles/vim ~/.vim
 ln -s ~/Github/dotfiles/vimrc ~/.vimrc
 ln -s ~/Github/dotfiles/gitconfig ~/.gitconfig
 
+echo "Update submodules"
+git submodule foreach git pull origin master --recurse-submodules
 
 echo "All done"
